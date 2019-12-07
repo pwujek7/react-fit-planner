1ec6fed2533257542cdcbb5255e39e07a3f6f54b
PW <pwujek7@gmail.com>
2019-12-07 09:10:57 +0100

    initial configuration #3 - styled-components

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: #47e6b1;
+  color: #313131;
+`;
 
 const Test = () => (
-  <div>test</div>
+  <StyledDiv>test</StyledDiv>
 );
 
 export default Test;
