e8f18d731120951e37dc5ff0204cd0955c7d9a75
PW <pwujek7@gmail.com>
2020-02-08 12:49:35 +0100

    added funcionality for fetching data

--- src/reducers/daysReducer.js
+++ src/reducers/daysReducer.js
@@ -3,13 +3,19 @@ import {
   ADD_DAY_SUCCESS,
   ADD_DAY_ERROR,
   EDIT_DAY,
-  DELETE_DAY
+  DELETE_DAY,
+  FETCH_ALL_DAYS_START,
+  FETCH_ALL_DAYS_SUCCESS,
+  FETCH_ALL_DAYS_ERROR
 } from '../actions/daysActions';
 
 const initialState = {
-  data: [],
-  postDataError: false,
-  postDataErrorMessage: ''
+  postDayError: false,
+  postDayErrorMessage: '',
+  fetchAllDaysLoading: false,
+  fetchAllDaysError: false,
+  fetchAllDaysErrorMessage: '',
+  data: []
 };
 
 const daysReducer = (state = initialState, action) => {
@@ -18,19 +24,36 @@ const daysReducer = (state = initialState, action) => {
       case ADD_DAY_SUCCESS: {
         const day = action.payload;
         draft.data.push(day);
-        draft.postDataError = false;
-        draft.postDataErrorMessage = '';
+        draft.postDayError = false;
+        draft.postDayErrorMessage = '';
         return;
       }
       case ADD_DAY_ERROR: {
-        draft.postDataError = true;
-        draft.postDataErrorMessage = action.payload.message;
+        draft.postDayError = true;
+        draft.postDayErrorMessage = action.payload.message;
         return;
       }
       case EDIT_DAY: {
         return;
       }
       case DELETE_DAY: {
+        return;
+      }
+      case FETCH_ALL_DAYS_START: {
+        draft.fetchAllDaysLoading = action.payload;
+        return;
+      }
+      case FETCH_ALL_DAYS_SUCCESS: {
+        draft.fetchAllDaysLoading = false;
+        draft.fetchAllDaysError = false;
+        draft.fetchAllDaysErrorMessage = '';
+        draft.data = action.payload;
+        return;
+      }
+      case FETCH_ALL_DAYS_ERROR: {
+        draft.fetchAllDaysLoading = false;
+        draft.fetchAllDaysError = true;
+        draft.fetchAllDaysErrorMessage = action.payload.message;
         return;
       }
       default: {
