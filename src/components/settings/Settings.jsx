1128dd292827c7b335b5b4c9207725bc788ad3b6
PW <pwujek7@gmail.com>
2020-03-27 11:23:12 +0100

    added styled components for Settings

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect, useSelector } from 'react-redux';
 import { Form } from 'formik';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { selectAuth } from '../../selectors/selectors';
 
@@ -11,6 +12,20 @@ import StyledAbsoluteContainer from '../common/styled/StyledAbsoluteContainer';
 
 import { updateEmail, updatePassword } from '../../actions/authActions';
 import { newEmailValidation, newPasswordValidation } from '../../schema/validation';
+
+const StyledSettings = styled.div`
+  display: grid;
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.s}) {
+    grid-template-columns: 1;
+    grid-template-rows: 2;
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.l}) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 1;
+  }
+`;
 
 const Settings = ({ newEmail, newPassword }) => {
   const [showEmail, setShowEmail] = useState(false);
@@ -35,64 +50,66 @@ const Settings = ({ newEmail, newPassword }) => {
   return (
     <StyledAbsoluteContainer>
       <p>Profile</p>
-      <div>
-        <span>e-mail: {email}</span><button type="button" onClick={handleShowEmail}>update</button>
-        {
-          showEmail
-          && (
-            <FormContainer
-              initialValues={{ email: '' }}
-              schema={newEmailValidation}
-              submitFunction={newEmail}
-            >
-              {
-                () => (
-                  <>
-                    <Form>
-                      <br />
-                      <TextInput id="emailID" name="email" type="text" label="New e-mail:" validate />
-                      <br />
-                      <button type="submit">Change</button>
-                    </Form>
-                    {
-                      updateEmailError && <span>{updateEmailErrorMessage}</span>
-                    }
-                  </>
-                )
-              }
-            </FormContainer>
-          )
-        }
-      </div>
-      <div>
-        <span>password: *****</span><button type="button" onClick={handleShowPassword}>update</button>
-        {
-          showPassword
-          && (
-            <FormContainer
-              initialValues={{ password: '' }}
-              schema={newPasswordValidation}
-              submitFunction={newPassword}
-            >
-              {
-                () => (
-                  <>
-                    <Form>
-                      <br />
-                      <TextInput id="passwordID" name="password" type="password" label="New password:" validate />
-                      <br />
-                      <button type="submit">Change</button>
-                    </Form>
-                    {
-                      updatePasswordError && <span>{updatePasswordErrorMessage}</span>
-                    }
-                  </>
-                )
-              }
-            </FormContainer>
-          )
-        }
-      </div>
+      <StyledSettings>
+        <div>
+          <span>e-mail: {email}</span><button type="button" onClick={handleShowEmail}>update</button>
+          {
+            showEmail
+            && (
+              <FormContainer
+                initialValues={{ email: '' }}
+                schema={newEmailValidation}
+                submitFunction={newEmail}
+              >
+                {
+                  () => (
+                    <>
+                      <Form>
+                        <br />
+                        <TextInput id="emailID" name="email" type="text" label="New e-mail:" validate />
+                        <br />
+                        <button type="submit">Change</button>
+                      </Form>
+                      {
+                        updateEmailError && <span>{updateEmailErrorMessage}</span>
+                      }
+                    </>
+                  )
+                }
+              </FormContainer>
+            )
+          }
+        </div>
+        <div>
+          <span>password: *****</span><button type="button" onClick={handleShowPassword}>update</button>
+          {
+            showPassword
+            && (
+              <FormContainer
+                initialValues={{ password: '' }}
+                schema={newPasswordValidation}
+                submitFunction={newPassword}
+              >
+                {
+                  () => (
+                    <>
+                      <Form>
+                        <br />
+                        <TextInput id="passwordID" name="password" type="password" label="New password:" validate />
+                        <br />
+                        <button type="submit">Change</button>
+                      </Form>
+                      {
+                        updatePasswordError && <span>{updatePasswordErrorMessage}</span>
+                      }
+                    </>
+                  )
+                }
+              </FormContainer>
+            )
+          }
+        </div>
+      </StyledSettings>
     </StyledAbsoluteContainer>
   );
 };
