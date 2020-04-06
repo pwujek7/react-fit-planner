e51cf7562c44d0135b8d1a7a72e1591af50fe0c7
PW <pwujek7@gmail.com>
2020-04-06 12:39:32 +0200

    added styles & styled components for Login form - desktop

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -17,12 +17,17 @@ import { signIn } from '../../actions/authActions';
 import { loginValidation } from '../../schema/validation';
 import { ICONS, COLORS } from '../../constants/icons';
 
-const StyledLoginContainer = styled(StyledAbsoluteContainer)`
+const StyledLoginContainer = styled.div`
   border: 2px solid ${({ theme }) => theme.color.darkBlue};
+  background-color: ${({ theme }) => theme.color.white};
 
   @media only screen and (min-width: ${({ theme }) => theme.breakpoint.s}) {
     width: 280px;
     padding: 30px 20px 40px 20px;
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.s}) {
+    padding: 30px 20px 20px 20px;
   }
 `;
 
@@ -30,17 +35,57 @@ const StyledLoginHeading = styled(StyledHeading)`
   background-color: ${({ theme }) => theme.color.white};
   padding: 0 10px;
   position: absolute;
-  top: -12px;
 
   & > svg {
     margin: 0 5px 0 0;
   }
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.s}) {
+    top: -10px;
+    left: 20px;
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.xl}) {
+    top: -13px;
+    left: 150px;
+  }
 `;
 
 const StyledLoginButton = styled(StyledButton)`
-  position: absolute;
-  bottom: -20px;
-  right: -20px;
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.s}) {
+    position: absolute;
+    bottom: -20px;
+    right: -20px;
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.xl}) {
+    position: relative;
+    bottom: 0;
+    right: 0;
+    margin: 15px 0 0 0;
+  }
+`;
+
+const StyledLoginPanel = styled.div`
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.s}) {
+    display: none;
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoint.xl}) {
+    display: block;
+    width: 280px;
+    height: 320px;
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 59%, rgba(45, 49, 77, 0.85) 100%),
+      url('src/assets/images/img-login.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    border: 2px solid ${({ theme }) => theme.color.darkBlue};
+    position: absolute;
+    top: 50%;
+    left: -140px;
+    transform: translateY(-50%);
+    z-index: -10;
+  }
 `;
 
 const Login = ({ login }) => {
@@ -48,35 +93,38 @@ const Login = ({ login }) => {
   const { loginError, loginErrorMessage } = auth;
 
   return (
-    <StyledLoginContainer>
-      <StyledLoginHeading>
-        <Icon icon={ICONS.ENTER} size="24" color={COLORS.DARKBLUE} />
-        Login
-      </StyledLoginHeading>
-      <FormContainer
-        initialValues={{
-          email: '',
-          password: ''
-        }}
-        schema={loginValidation}
-        submitFunction={login}
-      >
-        {
-          () => (
-            <>
-              <Form>
-                <TextInput id="emailID" name="email" type="text" label="e-mail:" validate />
-                <TextInput id="passwordID" name="password" type="password" label="password:" validate />
-                <StyledLoginButton type="submit">Login</StyledLoginButton>
-              </Form>
-              {
-                loginError && <span>{loginErrorMessage}</span>
-              }
-            </>
-          )
-        }
-      </FormContainer>
-    </StyledLoginContainer>
+    <StyledAbsoluteContainer>
+      <StyledLoginContainer>
+        <StyledLoginHeading>
+          <Icon icon={ICONS.ENTER} size="24" color={COLORS.DARKBLUE} />
+          Login
+        </StyledLoginHeading>
+        <FormContainer
+          initialValues={{
+            email: '',
+            password: ''
+          }}
+          schema={loginValidation}
+          submitFunction={login}
+        >
+          {
+            () => (
+              <>
+                <Form>
+                  <TextInput id="emailID" name="email" type="text" label="e-mail:" validate />
+                  <TextInput id="passwordID" name="password" type="password" label="password:" validate />
+                  <StyledLoginButton type="submit">Login</StyledLoginButton>
+                </Form>
+                {
+                  loginError && <span>{loginErrorMessage}</span>
+                }
+              </>
+            )
+          }
+        </FormContainer>
+      </StyledLoginContainer>
+      <StyledLoginPanel />
+    </StyledAbsoluteContainer>
   );
 };
 
