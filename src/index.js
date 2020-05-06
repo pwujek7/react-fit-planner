c05cff09832687c880a06d4945087e5998813085
PW <pwujek7@gmail.com>
2020-05-06 09:23:16 +0200

    moved react-router Routes & Switch to Routes component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import configureStore from './store/store';
-
-import { ROUTES } from './constants/routes';
-
 import Navigation from './components/navigation/Navigation';
-import PrivateRoute from './components/navigation/PrivateRoute';
-import LoadingIndicator from './components/common/LoadingIndicator';
+import Routes from './components/routes/Routes';
 import Theme from './theme/Theme';
 import GlobalStyle from './theme/globalStyles';
 
-const Login = lazy(() => import('./components/login/Login'));
-const Register = lazy(() => import('./components/register/Register'));
-const Page404 = lazy(() => import('./components/page404/Page404'));
+import configureStore from './store/store';
 
 const store = configureStore();
 
@@ -28,29 +17,7 @@ const App = () => (
     <GlobalStyle />
     <Router>
       <Navigation />
-      <Suspense fallback={<LoadingIndicator />}>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          {
-            ROUTES.map(route => (
-              <PrivateRoute
-                key={route.path}
-                exact={route.exact}
-                path={route.path}
-                component={route.component}
-              />
-            ))
-          }
-          <Route>
-            <Page404 />
-          </Route>
-        </Switch>
-      </Suspense>
+      <Routes />
     </Router>
   </Theme>
 );
