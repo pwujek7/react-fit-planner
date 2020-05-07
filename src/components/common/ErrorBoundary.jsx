33d76330e50ba84eac76368cf9cdfc77317ba357
PW <pwujek7@gmail.com>
2020-05-07 08:49:13 +0200

    added styles & styled components for ErrorBoundary

--- src/components/common/ErrorBoundary.jsx
+++ src/components/common/ErrorBoundary.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+import StyledAbsoluteContainer from './styled/StyledAbsoluteContainer';
+import StyledText from './styled/StyledText';
+
+import { COLOR, SIZE, WEIGHT } from '../../constants/styles';
+
+const StyledErrorBoundaryContainer = styled(StyledAbsoluteContainer)`
+  width: 260px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+`;
 class ErrorBoundary extends Component {
   state = {
     hasError: false
@@ -15,11 +29,34 @@ class ErrorBoundary extends Component {
   render() {
     const { hasError } = this.state;
     const { children } = this.props;
-    if (hasError) {
-      return <span>Something went wrong.</span>;
-    }
 
-    return children;
+    return hasError
+      ? (
+        <StyledErrorBoundaryContainer>
+          <StyledText
+            fontColor={COLOR.DARKBLUE}
+            fontSize={SIZE.XL}
+            fontWeight={WEIGHT.MEDIUM}
+          >
+            Something went wrong.
+          </StyledText>
+          <StyledText
+            fontColor={COLOR.GRAY}
+            fontSize={SIZE.M}
+            fontWeight={WEIGHT.NORMAL}
+          >
+            We are currently working on this.
+          </StyledText>
+          <StyledText
+            fontColor={COLOR.LIGHTGRAY}
+            fontSize={SIZE.S}
+            fontWeight={WEIGHT.NORMAL}
+          >
+            Please try again later...
+          </StyledText>
+        </StyledErrorBoundaryContainer>
+      )
+      : children;
   }
 }
 
