d328a3c73f001b3cb09646516727bcb82340aadc
PW <pwujek7@gmail.com>
2020-05-07 09:36:11 +0200

    added styles & styled components for Page404

--- src/components/page404/Page404.jsx
+++ src/components/page404/Page404.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+import StyledAbsoluteContainer from '../common/styled/StyledAbsoluteContainer';
+import StyledText from '../common/styled/StyledText';
+import StyledLink from '../common/styled/StyledLink';
+
+import { COLOR, SIZE, WEIGHT } from '../../constants/styles';
+
+const StyledPage404Container = styled(StyledAbsoluteContainer)`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Page404 = () => {
   const location = useLocation();
 
   return (
-    <div>
-      No match found for {location.pathname}
-    </div>
+    <StyledPage404Container>
+      <StyledText
+        fontColor={COLOR.GRAY}
+        fontSize={SIZE.M}
+        fontWeight={WEIGHT.NORMAL}
+      >
+        The page {location.pathname} you were looking could not be found!
+      </StyledText>
+      <StyledText
+        fontColor={COLOR.LIGHTGRAY}
+        fontSize={SIZE.M}
+        fontWeight={WEIGHT.NORMAL}
+      >
+        Try the <StyledLink to="/">home page.</StyledLink>
+      </StyledText>
+    </StyledPage404Container>
   );
 };
 
